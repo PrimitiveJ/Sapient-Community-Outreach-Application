d17e01744785e016f445b804918047db41e24d42
Primitive J <hokecj@gmail.com>
2022-07-13 19:30:50 -0400

    added input fields to registerpage.jsx

--- client/src/pages/RegisterPage/index.jsx
+++ client/src/pages/RegisterPage/index.jsx
@@ -1,5 +1,5 @@
-
-
+import React, { useState } from "react"
+import "./style.css"
 /*
 
     Page: Register Page
@@ -12,5 +12,60 @@
         * Business 
 
 */
+const regOptions = ['User', 'Organizer', 'Business'];
+
+function registerPage() {
+    const [state, setState] = useState({
+        fname: "",
+        lname: "",
+        email: "",
+      })
+    
+      const handleChange = e => {
+        setState({
+          // ...state,
+          [e.target.name]: e.target.value,
+        })
+      }
+    
+      return (
+        <div>
+          <h1>Register to get access to Sapient!</h1>
+          <form>
+            <label>
+              First Name:{" "}
+              <input
+                type="text"
+                name="fname"
+                value={state.fname}
+                onChange={handleChange}
+              />
+            </label>{" "}
+            <label>
+              Last Name:{" "}
+              <input
+                type="text"
+                name="lname"
+                value={state.lname}
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+                Email:{" "}
+              <input
+                type="text"
+                name="email"
+                value={state.lname}
+                onChange={handleChange}
+              />
+            </label>
+          </form>
+          <h5>
+            Name: {state.fname} {state.lname}
+          </h5>
+        </div>
+      )
+  }
 
 
+  export default RegisterPage
