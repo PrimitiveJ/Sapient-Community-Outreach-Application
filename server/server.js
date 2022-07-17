4d67b9615dd4db40d612620c5eaef9d80c3253ba
William Horn <williamjosephhorn@gmail.com>
2022-07-17 07:59:35 -0400

    added .env and express static middleware

--- server/server.js
+++ server/server.js
@@ -55,6 +55,10 @@ const PORT = process.env.PORT || 3001;
 // Use app middleware
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
