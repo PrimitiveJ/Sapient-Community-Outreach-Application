4c64e1021620285e0cf0b0d47686dbeec25ca433
William Horn <williamjosephhorn@gmail.com>
2022-07-17 07:53:38 -0400

    testing heroku launch

--- server/server.js
+++ server/server.js
@@ -56,7 +56,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 // 
